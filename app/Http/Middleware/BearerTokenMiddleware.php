2955da4327f04d56a9f6797db123cf160d8d8a5e
johny <batenko4@gmail.com>
2024-12-02 19:06:00 +0200

    added documentation for bearer token

--- app/Http/Middleware/BearerTokenMiddleware.php
+++ app/Http/Middleware/BearerTokenMiddleware.php
@@ -11,23 +11,21 @@ class BearerTokenMiddleware
      * Handle an incoming request.
      *
      * @param Request $request
-     * @param  \Closure  $next
+     * @param Closure $next
      * @return mixed
+     *
+     * Check request on isset token
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        // Получаем токен из заголовка Authorization
         $authHeader = $request->header('Authorization');
 
-        // Проверяем, что заголовок начинается с "Bearer "
         if (!$authHeader || !str_starts_with($authHeader, 'Bearer ')) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
 
-        // Извлекаем токен
         $token = substr($authHeader, 7);
 
-        // Сравниваем токен с тем, что указан в .env
         if ($token !== config('app.api_bearer_token')) {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
