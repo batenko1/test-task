972649d8dba5b45f774d4da1ceb8c71fc53601bf
johny <batenko4@gmail.com>
2024-12-12 16:32:20 +0200

    added rabbit mq

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $producer = app(\App\Kafka\ProducerService::class);
-    $producer->sendMessage('Test message from Laravel with Docker');
+//    $producer = app(\App\Kafka\ProducerService::class);
+//    $producer->sendMessage('Test message from Laravel with Docker');
+
+    $rabbitMQ = new \App\RabbitMQ\MessageService();
+    $rabbitMQ->publish('Hello, RabbitMQ! vlados', 'test-queue');
+    $rabbitMQ->close();
 });
